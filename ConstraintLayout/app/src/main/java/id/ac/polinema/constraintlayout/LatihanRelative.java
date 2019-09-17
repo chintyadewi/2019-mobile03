28a92e7edd5ef7fa824c2630ae469234f5ffdc11
chintyadewi <chintyadewi437@gmail.com>
2019-09-17 22:04:44 +0700

    tambah halaman latihan

---
+++ ConstraintLayout/app/src/main/java/id/ac/polinema/constraintlayout/LatihanRelative.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.constraintlayout;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LatihanRelative extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_latihan_relative);
+    }
+}
